1a084bb33cc0be44d871142704919f264ac35dec
abdellatif baiz <abaiz@sqli.com>
2023-05-15 14:46:43 +0100

    ajout des variation x y

--- src/test/java/com/sqli/marsrover/MarsRoverTest.java
+++ src/test/java/com/sqli/marsrover/MarsRoverTest.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
  class MarsRoverTest {
-    @Test
-    void moveTest() {
-        assertThat(move(1,2,'W','f')).isEqualTo("2 2 W");
-    }
 
-    String move(int x, int y, char direction, char command) {
-        return "2 2 W";
-    }
+         @Test
+         void moveTest() {
+             assertThat(move(1,2,'W','f')).isEqualTo("2 2 W");
+         }
+         @Test
+         void moveTestt(){
+             assertThat(move(2,2,'W','f')).isEqualTo("3 2 W");
+         }
+
+         String move(int x, int y, char direction, char command) {
+             return x+1+" 2 W";
+         }
 }
