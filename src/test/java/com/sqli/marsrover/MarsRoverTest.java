e5021976e8b4b7fe253aa7425383a9e16e8d740d
mbelfatmi <moussa.belfatmi@hotmail.com>
2023-05-15 15:18:12 +0100

    refactoring and introducing a record

--- src/test/java/com/sqli/marsrover/MarsRoverTest.java
+++ src/test/java/com/sqli/marsrover/MarsRoverTest.java
@@ -1,21 +1,28 @@
 package com.sqli.marsrover;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
- class MarsRoverTest {
+class MarsRoverTest {
 
 
-         @ParameterizedTest(name = "from x: {0} Y: {1} direction {2} command {3} to {4}")
-         @CsvSource({"1,2,W,f,2 2 W","2,2,W,f,3 2 W"})
-         void moveTest(int x,int y, char direction,char command,String expected) {
-             assertThat(move(x,y,direction,command)).isEqualTo(expected);
-         }
+    @ParameterizedTest(name = "from x: {0} Y: {1} direction {2} command {3} to {4}")
+    @CsvSource({
+            "1,2,W,f,2,2,W",
+            "2,2,W,f,3,2,W"}
+    )
+    void moveTest(int x, int y, char direction, char command, int newX, int newY, char newDir) {
+        Rover rover = new Rover(x, y, direction);
+        Rover result = move(rover, command);
+        assertThat(result).isEqualTo(new Rover(newX, newY, newDir));
+    }
 
-         String move(int x, int y, char direction, char command) {
-             return x+1+" 2 W";
-         }
+    private static Rover move(Rover rover, char command) {
+        return new Rover(rover.x() + 1, 2, 'W');
+    }
+
+    private static record Rover(int x, int y, char direction) {
+    }
 }
