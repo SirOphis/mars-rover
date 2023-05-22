7ec6a06f13b2026426c992897e277e348e4b56ca
AbdoulBaguiM <amohamadou@sqli.com>
2023-05-22 16:30:44 +0100

    move forward dans toutes les directions

--- src/main/java/com/sqli/marsrover/Rover.java
+++ src/main/java/com/sqli/marsrover/Rover.java
@@ -2,6 +2,28 @@ package com.sqli.marsrover;
 
 record Rover(int x, int y, char direction) {
     Rover moveForward() {
-        return new Rover(x() + 1, y(), 'W');
+        Rover rover = new Rover(x(), y(), direction);
+        rover = getRoverByDirection(rover);
+        return rover;
+    }
+
+    private Rover getRoverByDirection(Rover rover) {
+        switch (direction) {
+            case 'W':
+                rover = new Rover(x() - 1, y(), 'W');
+                break;
+            case 'N':
+                rover = new Rover(x(), y() + 1, 'N');
+                break;
+            case 'S':
+                rover = new Rover(x(), y() - 1, 'S');
+                break;
+            case 'E':
+                rover = new Rover(x() + 1, y(), 'E');
+                break;
+            default:
+                break;
+        }
+        return rover;
     }
 }
--- src/test/java/com/sqli/marsrover/MarsRoverTest.java
+++ src/test/java/com/sqli/marsrover/MarsRoverTest.java
@@ -2,22 +2,25 @@ package com.sqli.marsrover;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MarsRoverTest {
     @ParameterizedTest(name = "from x: {0} Y: {1} direction {2} command {3} to {4}")
     @CsvSource({
-            "1,2,W,2,2,W",
-            "2,2,W,3,2,W",
-            "2,1,W,3,1,W"}
-    )
+            "1,2,W,0,2,W",
+            "2,2,W,1,2,W",
+            "2,1,W,1,1,W",
+            "10,-5,N,10,-4,N",
+            "-4,7,S,-4,6,S",
+            "7,-2,E,8,-2,E",
+    })
+
     void moveForwardTest(int x, int y, char direction, int newX, int newY, char newDir) {
         Rover rover = new Rover(x, y, direction);
         Rover result = rover.moveForward();
         assertThat(result).isEqualTo(new Rover(newX, newY, newDir));
     }
-
-
-
 }
+
+
+
